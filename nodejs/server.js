82af0faff6c6e5d29da08f1318bb6aecfbe08dcb
Fips11 <nephilim11@hotmail.de>
2016-07-08 19:49:49 +0200

    Create server.js

---
+++ nodejs/server.js
@@ -0,0 +1,17 @@
+var readtemp = require("./test/tempread.js");
+var fs = require("fs");
+
+
+
+fs.stat('sensor.json', function(err, stat) {
+
+    if(err == null) {
+        var config = require('../sensor.json');
+        readtemp.loadConfig(config,function(){
+            readtemp.init(function(){
+                readtemp.startread(0/*function(){}*/);  
+                readtemp.CLIstart();
+                });
+        });   
+    } 
+});
