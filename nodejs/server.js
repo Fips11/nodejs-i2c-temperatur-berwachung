56edcdd22192605f92012f19828c8e2a1bd1315c
Fips11 <nephilim11@hotmail.de>
2016-07-08 19:54:00 +0200

    Update server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -6,7 +6,7 @@ var fs = require("fs");
 fs.stat('sensor.json', function(err, stat) {
 
     if(err == null) {
-        var config = require('../sensor.json');
+        var config = require('./sensor.json');
         readtemp.loadConfig(config,function(){
             readtemp.init(function(){
                 readtemp.startread(0/*function(){}*/);  
